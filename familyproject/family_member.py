950176662e9af481a94d2d1aa85804c7632fef88
Denys Nadvornyj <denysnadvornyj2007@gmail.com>
2026-05-16 16:41:05 +0200

    Add abstract and utility methods to FamilyMember class

--- familyproject/family_member.py
+++ familyproject/family_member.py
@@ -3,3 +3,13 @@ from abc import ABC, abstractmethod
 class FamilyMember(ABC):
     def __init__(self, name):
         self.name= name
+
+    @abstractmethod
+    def execute_order (self, target):
+        pass
+    
+    def collect_revenue (self):
+        pass
+
+    def intro (self):
+        print(f"Hi, I'm {self.name} and I'm a member of the family.")
